01bacc0a32b02940892e648fd0a5c29de5f3512a
aivvas <vassi.aiva@gmail.com>
2022-02-13 11:15:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ const URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 async function APIkey(){
         const response = await fetch(URL);
         const results = await response.json();
-        console.log(results)
-    
-    for (let i = 0; i < final.length; i++) 
-        console.log (final [i].rating);
+        console.log(results.length)
+
+    for (let i = 0; i < final.length; i++)
+    console.log (final [i].rating);
 }
 
 APIkey()
