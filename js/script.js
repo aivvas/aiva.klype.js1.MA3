81a68f417bc9ee1555f867a5f37ec225b46510ae
aivvas <vassi.aiva@gmail.com>
2022-02-13 11:07:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const getRemainder = (a,b) => a % b;
 
 //Question 2
-const URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=INSERTAPIKEYHERE";
+const URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=57e0d1680a184b218635d79c78dea433";
 
 async function APIkey(){
         const response = await fetch(URL);
